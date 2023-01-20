fc1e2f0ece50ddf60ac6f7ae7719794d24177f58
Teomat123 <105876337+Teomat123@users.noreply.github.com>
2023-01-20 17:44:16 -0500

    Update proyecto1.c

--- proyecto1.c
+++ proyecto1.c
@@ -2,25 +2,22 @@
 
 int main(){
 
-    FILE *archivo=NULL;
-    FILE *archivo2=NULL;
-
-    archivo=fopen("vector.txt","r+");
-    archivo2=fopen("vectormultiplicación,txt","w");
-
-    int a,b;
-    
-    if (archivo==NULL){
-        printf("No se puede abrir el archivo");
-        return -1;
-    }
-
-    for(int i=0; i<100; i++){
-        fscanf(archivo,"%d",&a);
-        b=a*3;
-        fprintf(archivo2,"%d\n",b);
-    }
-     fclose(archivo);
-     fclose(archivo2);
-     return 0;
+FILE *archivo=NULL;
+FILE *archivo2=NULL; int a;
+archivo=fopen ("vector .txt", "r");
+archivo2=fopen ("vector2.txt", "w");
+if (archivo==NULL){
+printf("No se puede abrir el archivo");
+return -1;
+while(Ifeof(archivo)){
+fscanf (archivo,
+"%d", &a);
+if (a%3==0){
+printf(archivo2, "modulo de 3\n".a);
+}else{
+fprintf (archivo2, "%d\n", a) ;
 }
+}
+fclose(archivo);
+fclose (archivo2);
+return 0;
