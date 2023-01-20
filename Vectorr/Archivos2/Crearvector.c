c521c5f2de3edf81cc22ca64a17950f433a7fa2f
Teomat123 <105876337+Teomat123@users.noreply.github.com>
2023-01-20 17:55:59 -0500

    Add files via upload

---
+++ Vectorr/Archivos2/Crearvector.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+FILE *archivo=NULL;
+FILE *archivo2=NULL; 
+int a;
+  
+archivo=fopen ("vector.txt","r");
+archivo2=fopen ("vector2.txt","w");
+  
+if (archivo==NULL){
+printf("No se puede abrir el archivo");
+return -1;
+}
+
+while(!feof(archivo)){
+fscanf (archivo,"%d", &a);
+if (a%3==0){
+fprintf (archivo2,"modulo de 3\n",a);
+}else{
+fprintf (archivo2,"%d\n",a) ;
+}
+}
+
+fclose(archivo);
+fclose(archivo2);
+return 0;
