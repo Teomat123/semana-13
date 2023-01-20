ea9e16a6b476c299cbe50a74e4abb5751d6f599b
Teomat123 <105876337+Teomat123@users.noreply.github.com>
2023-01-20 17:45:15 -0500

    Update proyecto1.c

--- proyecto1.c
+++ proyecto1.c
@@ -3,21 +3,24 @@
 int main(){
 
 FILE *archivo=NULL;
-FILE *archivo2=NULL; int a;
+FILE *archivo2=NULL; 
+int a;
+  
 archivo=fopen ("vector .txt", "r");
 archivo2=fopen ("vector2.txt", "w");
+  
 if (archivo==NULL){
 printf("No se puede abrir el archivo");
 return -1;
-while(Ifeof(archivo)){
-fscanf (archivo,
-"%d", &a);
+}
+while(!feof(archivo)){
+fscanf (archivo,"%d", &a);
 if (a%3==0){
-printf(archivo2, "modulo de 3\n".a);
+fprintf (archivo2, "modulo de 3\n".a);
 }else{
 fprintf (archivo2, "%d\n", a) ;
 }
 }
 fclose(archivo);
-fclose (archivo2);
+fclose(archivo2);
 return 0;
